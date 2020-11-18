06ab6c681682e4d36f8000e9c3142907d53d397b
1gilmar <1gilmar@gmail.com>
2020-11-18 09:40:52 -0300

    adicionado buttao para incrementar valor usando useState do React

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import {useState} from 'react';
+
 function Home(){
     return (
     <div> 
         <div> HOME PRINCIPAL NEXTJS</div>
+        
+
+        <Contador/>
+        
         <div> 
             Teste finalizado.
         </div>
@@ -17,3 +23,18 @@ export default function Home() {
      return <div>Home2</div>
  }
  */
+
+ function Contador(){
+    const [contador, setContador] = useState(1);
+
+    function incrementarContador(){
+        setContador(contador +1);
+    }
+
+    return (
+        <div>
+            <div> contador {contador}</div>
+            <button onClick={incrementarContador}> Incrementar contador</button>
+        </div>
+    )
+ }
